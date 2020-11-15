4ed4bf8d6f33c95c8425e9d734b421b1cc0c66af
HoopeMax <nadezhda-maksimova-01@mail.ru>
2020-11-15 18:50:48 +0300

    Задание 1 выполнила

--- V_mat3/Source.cpp
+++ V_mat3/Source.cpp
@@ -3,11 +3,15 @@
 //Метод Гаусса
 void Metod_Gays(int m, int n, float** matrix);
 //Вывод матриц
-void InputMat(int n, int m, float** matrix); 
+void OutputMat(int n, int m, float** matrix); 
 //Умножение матриц
 void MatrixMul(int n, int m, float** matA, float** matA2, float** newmatA);
 //Создание AX=B и A^3X=B
 void MatAB(int n, int mA, float* matB, int mAB, float** matA, float** matAB);
+//Ввод вектора В и У
+void InputVek(int n, float* vek);
+// У(т)*А^3*У
+void Resul(int n, float* vekY, float** newmatA3);
 
 int main()
 {
@@ -24,15 +28,13 @@ int main()
     //Вектор В
     float* matB = new float[n];
 
+    //Вектор У
+    float* matY = new float[n];
+
     //Матрица АB
     float** matAB = new float* [n];
     for (i = 0; i < n; i++)
         matAB[i] = new float[mAB];
-
-    //Матрица А^3B
-    float** matA3B = new float* [n];
-    for (i = 0; i < n; i++)
-        matA3B[i] = new float[mAB];
 
     //Матрица А
     float** matA = new float* [n];
@@ -62,41 +64,39 @@ int main()
     
     // Bектор В
     std::cout << std::endl << "Введите  В: " << std::endl;
-    for (i = 0; i < n; i++)
-    {
-        std::cin >> matB[i];
-    }
+    InputVek(n, matB);
+
+    // Bектор Y
+    std::cout << std::endl << "Введите  Y: " << std::endl;
+    InputVek(n, matY);
 
     //Создаем АВ
     MatAB(n, mA, matB, mAB, matA, matAB);
 
     //выводим массив АХ=В
     std::cout << "Матрица АХ=В: " << std::endl;
-    InputMat(n, mAB, matAB);
+    OutputMat(n, mAB, matAB);
  
     MatrixMul(n, mA, matA, matA, newmatA);
     MatrixMul(n, mA, matA, newmatA, newmatA3);
 
-    //Создаем А3В
-    MatAB(n, mA, matB, mAB, newmatA3, matA3B);
-
     std::cout << "Гаус АХ=В: " << std::endl;
     Metod_Gays(mAB, n, matAB);
 
-    std::cout << "Матрица А2=: " << std::endl;
-    InputMat(n, mA, newmatA);
+    //Создаем А3В
+    MatAB(n, mA, matB, mAB, newmatA3, matAB);
+
     std::cout << "Матрица А3=: " << std::endl;
-    InputMat(n, mA, newmatA3);
-    std::cout << "Матрица А3Х=В: " << std::endl;
-    InputMat(n, mAB, matA3B);
+    OutputMat(n, mA, newmatA3);
+    std::cout << "Матрица А^3Х=В: " << std::endl;
+    OutputMat(n, mAB, matAB);
 
-    std::cout << "Гаус А3Х=В: " << std::endl;
-    Metod_Gays(mAB, n, matA3B);
+    std::cout << "Гаус А^3Х=В: " << std::endl;
+    Metod_Gays(mAB, n, matAB);
 
-    delete[] matAB;
-    delete[] matA;
-    delete[]newmatA;
-    delete[]newmatA3;
+    Resul(n, matY, newmatA3);
+
+    delete[] matAB, matA, newmatA, newmatA3, matB, matY;
 
     system("pause");
 
@@ -137,7 +137,7 @@ void Metod_Gays(int m, int n, float** matrix)
     std::cout << std::endl;
 }
 
-void InputMat(int n, int m, float** matrix)
+void OutputMat(int n, int m, float** matrix)
 {
     std::cout << std::endl;
     for (int i = 0; i < n; i++)
@@ -148,6 +148,7 @@ void InputMat(int n, int m, float** matrix)
     }
     std::cout << std::endl;
 }
+
 void MatrixMul(int n, int m, float** matA, float** matA2, float** newmatA)
 {
     for (int i = 0; i < n; i++)
@@ -159,7 +160,6 @@ void MatrixMul(int n, int m, float** matA, float** matA2, float** newmatA)
         }
 }
 
-//A+B
 void MatAB(int n, int mA, float* matB, int mAB, float** matA, float** matAB)
 {
     
@@ -174,4 +174,45 @@ void MatAB(int n, int mA, float* matB, int mAB, float** matA, float** matAB)
     {
         matAB[i][mAB-1] = matB[i];
     }
+}
+
+void InputVek(int n, float* vek)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> vek[i];
+    }
+}
+
+void Resul(int n, float* vekY, float** newmatA3)
+{
+    float* vek = new float[n];
+
+    float result = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        vek[i] =0;
+    }
+ 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            vek[i] = vek[i] + (newmatA3[j][i] * vekY[j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << std::endl << vek[i] << std::endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        result = result + (vek[i] * vekY[i]);
+    }
+
+    std::cout << std::endl << result << std::endl;
+
 }
